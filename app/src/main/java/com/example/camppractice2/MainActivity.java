836d21dfa514d22ec760698facd1d29c6dbee9c2
ParkHyebeen <70693435+ParkHyebeen@users.noreply.github.com>
2021-07-01 13:10:09 +0900

    add files

--- app/src/main/java/com/example/camppractice2/MainActivity.java
+++ app/src/main/java/com/example/camppractice2/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.camppractice2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textview;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout);
+
+        textview = findViewById(R.id.txtCount);
+
+
+    }
+
+    public void onToastClick(View view){
+        Toast.makeText(this, "Hello Toast", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void onCountClick(View view){
+
+        count++;
+        textview.setText(count+"");
+
     }
 }
